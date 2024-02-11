73f299bfa457b7472081d6666c71864f95fdc4f9
JonyWizard <moulaf4@gmail.com>
2024-02-10 20:50:36 -0600

    edited the flask server thing

--- frontEnd/scripts/script.py
+++ frontEnd/scripts/script.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+import sqlite3
 
 app = Flask(__name__)
+
 
 @app.route('/submit-form', methods=['POST'])
 def handle_data():
@@ -11,4 +13,8 @@ def handle_data():
     return 'Data received'
 
 if __name__ == '__main__':
+    sqliteConnection = sqlite3.connect('../../DB/COP_Sustainability.db')
+    cursor = sqliteConnection.cursor()
+
     app.run(debug=True)
+    sqliteConnection.close()
