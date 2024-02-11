e0bca8b594b27067048e7b66d55bab6ea819d94b
JonyWizard <moulaf4@gmail.com>
2024-02-10 21:06:12 -0600

    added SQL query to flask script.py

--- frontEnd/scripts/script.py
+++ frontEnd/scripts/script.py
@@ -3,18 +3,25 @@ import sqlite3
 
 app = Flask(__name__)
 
+DB_path = "../../DB/COP_Sustainability.db"
+
+
+def insert_recipient(first_name, email):
+    with sqlite3.connect(DB_path) as conn:
+        c = conn.cursor()
+        c.execute(f"INSERT INTO EmailList (name, email) VALUES ('{first_name}', '{email}')")
+        conn.commit()
+
 
 @app.route('/submit-form', methods=['POST'])
 def handle_data():
     first_name = request.form['firstname']
     email = request.form['email']
-    print(f"Name: {first_name}, Email: {email}")
-    # Add database interaction here
+
+    insert_recipient(first_name, email)
+
     return 'Data received'
 
-if __name__ == '__main__':
-    sqliteConnection = sqlite3.connect('../../DB/COP_Sustainability.db')
-    cursor = sqliteConnection.cursor()
 
+if __name__ == '__main__':
     app.run(debug=True)
-    sqliteConnection.close()
